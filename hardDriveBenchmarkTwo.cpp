b12b43b127408f0344e16ca0f9c1472ce8f17b77
Kryptonite <102353731+biraj-dahal@users.noreply.github.com>
2024-03-12 15:33:55 -0400

    Print the benchmarking in different colors.

--- hardDriveBenchmarkTwo.cpp
+++ hardDriveBenchmarkTwo.cpp
@@ -50,3 +50,12 @@ int main() {
     auto end = chrono::high_resolution_clock::now();
     auto diff = chrono::duration_cast<std::chrono::duration<double> >(end - start);
     totalTime = diff.count();
+
+    // Print benchmarking data in different colors
+    cout << ANSI_COLOR_YELLOW << "Benchmark 5:" << endl << ANSI_COLOR_RESET;
+    cout << ANSI_COLOR_GREEN << "Hard Drive benchmark 2" << endl << ANSI_COLOR_RESET;
+    cout << ANSI_COLOR_RED << "Which includes reading from, and writing to file, 10000 bytes each time' is: " << totalTime << " seconds" << ANSI_COLOR_RESET << endl;
+
+    return 0;
+}
+
