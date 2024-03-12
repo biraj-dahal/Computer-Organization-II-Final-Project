ac4847d05145b14b1298deb75adf5c87bfbcc303
Kryptonite <102353731+biraj-dahal@users.noreply.github.com>
2024-03-12 15:00:05 -0400

    Set initial values, and start the timer of the operations.

--- floatingBenchmark.cpp
+++ floatingBenchmark.cpp
@@ -9,3 +9,11 @@ using namespace std;
 #define ANSI_COLOR_GREEN   "\x1b[32m"
 #define ANSI_COLOR_YELLOW  "\x1b[33m"
 #define ANSI_COLOR_RESET   "\x1b[0m"
+
+int main() {
+    double totalTime;
+    int32_t dummyValue = 7;
+    int32_t res = 0;
+
+    // 10^10 additions (of integer constants)
+    auto start = chrono::high_resolution_clock::now();
