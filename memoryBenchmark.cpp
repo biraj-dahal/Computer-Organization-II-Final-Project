aa5e8577a54e577f88063cfe35f04d053a0c1077
Kryptonite <102353731+biraj-dahal@users.noreply.github.com>
2024-03-12 15:11:02 -0400

    Print benchmarking in different colors, and delete the array.

--- memoryBenchmark.cpp
+++ memoryBenchmark.cpp
@@ -31,3 +31,14 @@ int main() {
     auto end = chrono::high_resolution_clock::now();
     auto diff = chrono::duration_cast<std::chrono::duration<double> >(end - start);
     totalTime = diff.count();
+
+    // Print benchmarking data in different colors
+    cout << ANSI_COLOR_YELLOW << "Benchmark 3:" << endl << ANSI_COLOR_RESET;
+    cout << ANSI_COLOR_GREEN << "Memory benchmark" << endl << ANSI_COLOR_RESET;
+    cout << ANSI_COLOR_RED << "Which includes reading from, and writing to array, 4 bytes each time' is: " << totalTime << " seconds" << ANSI_COLOR_RESET << endl;
+    
+    delete[] myArr;
+
+    return 0;
+}
+
