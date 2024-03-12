ccbabfd2f0194d9939ed097af9072bbe42a469b2
Kryptonite <102353731+biraj-dahal@users.noreply.github.com>
2024-03-12 14:37:38 -0400

    Add color codes definitions to print later

--- integerBenchmark.cpp
+++ integerBenchmark.cpp
@@ -2,3 +2,11 @@
 #include <string>
 #include <cstdint>
 #include <chrono>
+
+using namespace std;
+
+// ANSI escape codes for colors
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_YELLOW  "\x1b[33m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
